7ba2ecafa702840b2cda94c22d5acd309348d9bc
Andrew Kim <andrew@socsym.com>
2014-04-29 07:30:00 -1000

    added a test class

---
+++ AKTest.h
@@ -0,0 +1,13 @@
+//
+//  AKTest.h
+//  Git Test
+//
+//  Created by Andrew Kim on 4/29/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AKTest : NSObject
+
+@end
---
+++ AKTest.m
@@ -0,0 +1,13 @@
+//
+//  AKTest.m
+//  Git Test
+//
+//  Created by Andrew Kim on 4/29/14.
+//
+//
+
+#import "AKTest.h"
+
+@implementation AKTest
+
+@end
